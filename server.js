93d692eba525e12168ace8f9a1f30df3f27d472d
Ian Cover <iancover@gmail.com>
2020-08-18 03:57:25 -0500

    added controllers

--- server.js
+++ server.js
@@ -10,6 +10,14 @@ dotenv.config({ path: './config/config.env'});
 
 const app = express();
 
+const logger = (req, res, next) => {
+  req.hello = 'Hello World';
+  console.log('Middleware ran');
+  next();
+};
+
+app.use(logger);
+
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
