039958d94b344c9312efd326ad9544ba0856783c
Ian Cover <iancover@gmail.com>
2020-08-18 07:12:37 -0500

    added colors pkg to style console logs

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
 const connectDB = require('./config/db')
 
 // Load environment variables
@@ -26,13 +27,13 @@ app.use('/api/v1/bootcamps', bootcamps);
 const PORT = process.env.PORT || 5000;
 const server = app.listen(
   PORT, 
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
 );
 
 // Handle unhandled promise rejections
   // example: when changing pwd on db host
 process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`);
+  console.log(`Error: ${err.message}`.red);
   // Close server & exit process
   server.close(() => process.exit(1));
 });
