3aaeccec5751e2068aaee55e058d8b39d856b2df
Ian Cover <iancover@gmail.com>
2020-08-21 23:59:16 -0500

    Add geocoder middleware

--- middleware/error.js
+++ middleware/error.js
@@ -1,11 +1,34 @@
+const ErrorResponse = require('../utils/errorResponse');
 
 const errorHandler = (err, req, res, next) => {
-  // log to console for developer
-  console.log(err.stack.red);
+  let error = { ...err };
 
-  res.status(err.statusCode || 500).json({ 
+  error.message = err.message;
+
+  // log to console for developer
+  console.log(err);
+
+  // Mongoose: Bad ObjectId
+  if (err.name === 'CastError') {
+    const message = `Resource not found with id of ${err.value}`;
+    error = new ErrorResponse(message, 404);
+  }
+
+  // Mongoose: Duplicate Key
+  if (err.code === 11000) {
+    const message = 'Duplicate field value entered';
+    error = new ErrorResponse(message, 400);
+  }
+
+  // Mongoose: Validation Error
+  if(err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map(val => val.message);
+    error = new ErrorResponse(message, 400);
+  }
+
+  res.status(error.statusCode || 500).json({ 
     success: false, 
-    error: err.message || 'Server Error'
+    error: error.message || 'Server Error'
   });
 };
 
--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
+const geocoder = require('../utils/geocoder');
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -39,12 +41,10 @@ const BootcampSchema = new mongoose.Schema({
   location: {
     type: {
       type: String,
-      enum: ['Point'], 
-      required: false
+      enum: ['Point']
     },
     coordinates: {
       type: [Number],
-      required: false,
       index: '2dsphere'
     },
     formattedAddress: String,
@@ -96,6 +96,30 @@ const BootcampSchema = new mongoose.Schema({
   }
 });
 
+// Create Bootcamp Slug from the Name
+BootcampSchema.pre('save', function(next) {
+  // console.log('Slugify ran', this.name);
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// Geocode & Create Location Field
+BootcampSchema.pre('save', async function(next) {
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: 'Point',
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode
+  };
+  next();
+});
+
+this.address = undefined;
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
 
