4ecd4620202f495aa325e8090ae7485bc4cedef7
Ian Cover <iancover@gmail.com>
2020-09-02 04:28:32 -0500

    Add crud for course ctrls and routes

--- routes/courses.js
+++ routes/courses.js
@@ -1,15 +1,29 @@
 
 const express = require('express');
 
-const { getCourses } = require('../controllers/courses');
+const { 
+  getCourses, 
+  getCourse, 
+  addCourse, 
+  updateCourse, 
+  deleteCourse 
+} = require('../controllers/courses');
 
 // Router Setup
   // - must merge url params to pass bootcamp id from bootcamps router
   //   so controllers can get the courses of a bootcamp
 const router = express.Router({ mergeParams: true });
 
+// Routes
 router
   .route('/')
-  .get(getCourses);
+  .get(getCourses)
+  .post(addCourse);
+
+router
+  .route('/:id')
+  .get(getCourse)
+  .put(updateCourse)
+  .delete(deleteCourse);
 
 module.exports = router;
