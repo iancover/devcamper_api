bb1f82a13c649a2b3996555e5d489e0b5a075cf7
Ian Cover <iancover@gmail.com>
2020-09-02 01:23:14 -0500

    fix comments on seeder module

--- seeder.js
+++ seeder.js
@@ -1,26 +1,33 @@
 // Data Seeder
   // this module connects to db, seeds and destroys data from '/_data' 
-  // fs - node module that provides API to interact with file system
-  // mongoose - to connect to db
-  // dotenv - import env variables
-  // colors - to style terminal logs
-  // Bootcamp - import models
-  // bootcamps - reads file system accessing /_data/bootcamps.json synchronously
-  // importData - import data from there into db asynchronously
-  // deleteData - destroy data from db
-  // so before running app we wanna temp data into db running 'node seeder -i'
-  // process.argv[] -  is an array ['node', 'seeder', '-i']
-  // so if process.argv[2] === '-i' import data or if '-d' destroy data
+// --------------------------------------------------
+
+// Modules Imported
+    //  fs - node module that provides API to interact with file system
+    //  mongoose - to connect to db
+    //  dotenv - import env variables
+    //  colors - to style terminal logs
 const fs = require('fs');
 const mongoose = require('mongoose');
 const colors = require('colors');
 const dotenv = require('dotenv');
 
+// Config Env Variables
+  // dotenv.config() - to use env variables
 dotenv.config({ path: './config/config.env' });
 
+// Models
+  // Bootcamp - import bootcamps schema/model
+  // Course - import courses schema/model
 const Bootcamp = require('./models/Bootcamp');
 const Course = require('./models/Course');
 
+// Connect to DB
+  // mongoose.connect(url, options) - pass the env db url and options
+  // make sure to pass options: 
+  //  'useNewUrlParser' - or mongoose won't parse url data correctly & cant connect
+  //  'useCreateIndex' & 'useUnifiedTopology' - not familiar with these
+  //  'useFindAndModify' - or this one, and this one must be false
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useCreateIndex: true,
@@ -28,10 +35,27 @@ mongoose.connect(process.env.MONGO_URI, {
   useUnifiedTopology: true
 });
 
+// Synch Import Data
+  // indicating dir, use 'fs' to synchronously get data
+  // JSON.parse() - data is imported as string, so must parse as JSON 
+  // fs.readFileSync() - read file system synch, indicate dir & utf type
+  // __dirname - use this variable in case you change name of directory + dir
 const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8'));
 const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8'));
 
-const importData = async () => {
+// Async Seed/Delete Data
+  // seed/destroy data using try/catch for error handling, colored logs & exit process, or log error
+  // importData()
+    // async so must use 'await'
+    // Model.create() - creates records with data imported using models
+  // deleteData()
+    // async so must use 'await'
+    // Model.deleteMany() - when passed empty deletes all of them
+  // process.argv[] - specifies arguments to run each function
+    // process = running module with node 'node seeder'
+    // argv = makes array of characters after process, so we specify
+    //        if index 2 is '-s' runs 'seedData()', if '-d' runs 'deleteData()'
+const seedData = async () => {
   try {
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
@@ -53,8 +77,8 @@ const deleteData = async () => {
   }
 };
 
-if (process.argv[2] === '-i') {
-  importData();
+if (process.argv[2] === '-s') {
+  seedData();
 } else if (process.argv[2] === '-d') {
   deleteData();
 }
